058e6d8fd49fa1afa0dc2b94628376a78e172b72
nthduc95 <nthduc95@gmail.com>
2023-10-12 09:44:15 +0700

    test ci backend

--- backend/test_app.py
+++ backend/test_app.py
@@ -6,6 +6,7 @@ def test_movies_endpoint_returns_200():
     with app.test_client() as client:
         status_code = os.getenv("FAIL_TEST", 200)
         response = client.get("/movies/")
+        print 'test ci'
         assert response.status_code == status_code
 
 
